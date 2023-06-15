c1aaeab3116affee6baf02c5304edd9c9b0e8619
salahakrim <salahakrim23@gmail.com>
2023-06-15 08:26:05 -0700

    printf

---
+++ 0x00-hello_world/5-print.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+/**
+ * main - A program that prints a line using the printf function
+ */
+int main()
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return 0;
+}

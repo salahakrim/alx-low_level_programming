1192e8aa8680b9f22c89518c349176866a7680e1
salahakrim <salahakrim23@gmail.com>
2023-06-15 08:20:17 -0700

    fixing

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 /**
  * main - A program that prints a line with puts functon
- * return: 0 (Success)
+ * Return: 0 (Success)
  */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
+

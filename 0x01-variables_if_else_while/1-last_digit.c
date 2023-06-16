30ae3c99985e634caee6ca9a86a9d4758cf49c8e
salahakrim <salahakrim23@gmail.com>
2023-06-16 15:03:54 -0700

    positive or negative

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -2,25 +2,26 @@
 #include <stdlib.h>
 #include <time.h>
 
-int main() {
-    int n;
+int main(void)
+{
+int n;
 
-    srand(time(0));  // Seeds the random number generator with the current time
-
-    n = rand();  // Assigns a random number to the variable n
-
-    int lastDigit = n % 10;  // Extracts the last digit of n
-
-    printf("Last digit of %d is ", n);
-
-    if (lastDigit > 5) {
+srand(time(0)); 
+n = rand();
+int lastDigit = n % 10;
+printf("Last digit of %d is ", n);
+if (lastDigit > 5)
+{
         printf("greater than 5\n");
-    } else if (lastDigit == 0) {
+}
+else if (lastDigit == 0)
+{
         printf("0\n");
-    } else {
+}
+else
+{
         printf("less than 6 and not 0\n");
-    }
-
-    return 0;
+}
+return (0);
 }
 

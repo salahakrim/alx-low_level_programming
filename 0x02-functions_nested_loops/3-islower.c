c81b08efa2fe4ba79bf19aac3cba3a57202de0d6
salahakrim <salahakrim23@gmail.com>
2023-06-20 15:39:06 -0700

    fixing

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -3,7 +3,7 @@
 /**
  * _islower - Checks for lowercase character
  * @c: The character to be checked
- * Return: 1 for lowercase character or 0 for anything else 
+ * Return: 1 for lowercase character or 0 for anything else
  */
 
 int _islower(int c)
@@ -11,7 +11,6 @@ int _islower(int c)
 {
 	if (c >= 97 && c <= 122)
 	{
-		
 	return (1);
 	}
 	return (0);
